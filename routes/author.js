77964632888dcb1c3cb65ec07fe19a75c4517769
Implication <IamTrajon@gmail.com>
2020-03-01 06:19:26 -0800

    Adding Books Route

--- routes/author.js
+++ routes/author.js
@@ -10,7 +10,6 @@ router.get("/", async (req, res) => {
   }
   try {
     const authors = await Author.find(searchOptions);
-    console.log(authors);
     res.render("authors/index", {
       authors: authors,
       searchOptions: req.query
